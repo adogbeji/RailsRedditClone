26e8210a0bd6d27640bbf49e64494f6e17f82ba8
adogbeji <baka_1993@yahoo.co.uk>
2024-04-24 18:28:14 +0000

    Modify subreddits model & controller

--- app/controllers/subreddits_controller.rb
+++ app/controllers/subreddits_controller.rb
@@ -60,7 +60,7 @@ class SubredditsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_subreddit
-      @subreddit = Subreddit.find(params[:id])
+      @subreddit = Subreddit.friendly.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
